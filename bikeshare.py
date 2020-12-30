43071ca98fac9bcd4941b6ec5d1e2bc706903ccc
Jeff Koo <koo.jeff@gmail.com>
2020-12-31 03:09:06 +0900

    Changed the wrong city name message

--- bikeshare.py
+++ bikeshare.py
@@ -19,7 +19,7 @@ def get_filters():
     # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
     city = input("Input for city (chicago, new york city, washington):").lower()
     while(city not in ['chicago','new york city','washington']):
-        print(" ******* WRONG CITY NAME *******")
+        print(" ******* Wrong City Name *******")
         city = input("Input for city (chicago, new york city, washington):").lower()
 
     # get user input for month (all, january, february, ... , june)
